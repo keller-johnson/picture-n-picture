12fd88b2337f3391ffc7929ff16bb5188936c0f7
keller-johnson <keller.s.johnson@gmail.com>
2022-11-22 20:51:13 -0800

    powerful web api screen capture which easily creates picture in picture beyond the scope of the browser. Impressed

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("test");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+//Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.onplay();
+    };
+  } catch (error) {
+    //catch errors
+    console.log("opsie", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  //Display Button
+  button.disabled = true;
+  //Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  //Reset Button
+  button.disabled = false;
+});
+//on load
+selectMediaStream();
